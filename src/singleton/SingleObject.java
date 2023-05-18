09dc6d24147541e44d6800fea85f129251b5a741
Lucas Almeida Coelho <89714517+lucasacoelho@users.noreply.github.com>
2023-05-17 23:08:13 -0300

    Add files via upload

---
+++ src/singleton/SingleObject.java
@@ -0,0 +1,21 @@
+package singleton;
+
+public class SingleObject {
+
+    private static SingleObject instance;
+    
+    private SingleObject() {
+        
+    }
+    
+    public static SingleObject getInstance() {
+        if (instance == null) {
+            instance= new SingleObject();
+        }
+        return instance;
+    }
+    
+    public void showMessage(){
+        System.out.println("Single Object:" + this);
+    }
+}
